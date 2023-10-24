8953806cead81a355ef28b371f6058132e8f4d4a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 10:13:36 +0100

    chore: add styles to form container comp

--- src/ui/FormContainer.jsx
+++ src/ui/FormContainer.jsx
@@ -1,3 +1,12 @@
 import styled from "styled-components";
+import { clampBuilder } from "../Styles/clampBuilder";
 
-export const FormContainer = styled.div``;
+export const FormContainer = styled.div`
+  padding-block: ${() => clampBuilder(350, 1200, 2, 3)};
+  border-radius: ${() => clampBuilder(350, 1200, 1, 1.5)};
+  padding-inline: ${() => clampBuilder(350, 1200, 1.5, 2.5)};
+
+  background-color: var(--color-blue-dark);
+
+  width: ${() => clampBuilder(350, 1200, 25, 45)};
+`;
