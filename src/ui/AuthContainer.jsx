f46c433146d5e5cd04493f3643ba693f1fa5e6c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 10:37:36 +0100

    chore: add auth container in a separate component

---
+++ src/ui/AuthContainer.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AuthContainer = styled.div`
+  height: 100vh;
+
+  display: grid;
+
+  place-items: center;
+
+  background-color: var(--color-primary);
+`;
