82767e677901d9b680c543147f582158b0c5a7e0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 07:51:54 +0100

    chore: create the form componenet

--- src/ui/Form.jsx
+++ src/ui/Form.jsx
@@ -1,5 +1,10 @@
-function Form() {
-  return <div>Form</div>;
-}
+import styled from "styled-components";
+import { clampBuilder } from "../Styles/clampBuilder";
+
+const Form = styled.form`
+  padding-block: ${() => clampBuilder(350, 1200, 2, 3)};
+
+  padding-inline: ${() => clampBuilder(350, 1200, 1.5, 2.5)};
+`;
 
 export default Form;
