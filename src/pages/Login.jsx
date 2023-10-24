a69e8a7789c908c8b85a0f32e75aaddf3446c9bf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 08:17:09 +0100

    chore: finished the login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+import LoginForm from "../features/LoginForm";
+
+const StyledLogin = styled.section`
+  height: 100vh;
+
+  display: grid;
+
+  place-items: center;
+
+  background-color: var(--color-blue-dark);
+`;
+
 function Login() {
-  return <div>The login page</div>;
+  return (
+    <StyledLogin>
+      <LoginForm />
+    </StyledLogin>
+  );
 }
 
 export default Login;
