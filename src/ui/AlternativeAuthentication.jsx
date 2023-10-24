c59e53fee9e3aea8f12620f418a103bced86696d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 07:44:18 +0100

    chore: create a component for alt auth

---
+++ src/ui/AlternativeAuthentication.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import { clampBuilder } from "../Styles/clampBuilder";
+import styled from "styled-components";
+
+const StyledAuth = styled.div`
+  display: flex;
+
+  justify-content: center;
+
+  align-items: center;
+
+  font-size: ${() => clampBuilder(350, 1200, 0.8, 1.6)};
+
+  column-gap: ${() => clampBuilder(350, 1200, 1, 2)};
+
+  & > a {
+    color: var(--color-red);
+  }
+`;
+
+function AlternativeAuthentication({ method }) {
+  const methodText =
+    method === "Login" ? "Don't have an account?" : "Already have an account?";
+
+  const to = method === "login" ? "/signup" : "/login";
+
+  return (
+    <StyledAuth>
+      <span> {methodText} </span>
+      <NavLink to={to}> {method} </NavLink>
+    </StyledAuth>
+  );
+}
+
+export default AlternativeAuthentication;
