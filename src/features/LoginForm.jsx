b4e025eb2b48dfb5fcdedf72ada7a1eab73b24e7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 08:13:37 +0100

    chore: complete the login form styles

--- src/features/LoginForm.jsx
+++ src/features/LoginForm.jsx
@@ -1,13 +1,38 @@
 import styled from "styled-components";
+import { clampBuilder } from "../Styles/clampBuilder";
+import Heading from "../ui/Heading";
+import Form from "../ui/Form";
+import FormRow from "../ui/FormRow";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+import AlternativeAuthentication from "../ui/AlternativeAuthentication";
 
 const StyledForm = styled.div`
   padding-block: ${() => clampBuilder(350, 1200, 2, 3)};
-
+  border-radius: ${() => clampBuilder(350, 1200, 1, 1.5)};
   padding-inline: ${() => clampBuilder(350, 1200, 1.5, 2.5)};
 `;
 
 function LoginForm() {
-  return <div></div>;
+  return (
+    <StyledForm>
+      <Heading> Login </Heading>
+
+      <Form>
+        <FormRow>
+          <Input placeholder="Email address" />
+        </FormRow>
+
+        <FormRow>
+          <Input placeholder="Password" />
+        </FormRow>
+
+        <Button> Login to your account </Button>
+
+        <AlternativeAuthentication method="Login" />
+      </Form>
+    </StyledForm>
+  );
 }
 
 export default LoginForm;
