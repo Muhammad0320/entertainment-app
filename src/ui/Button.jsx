da5bfd58434d89bc03469d9d0334825a3228240f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-24 08:11:00 +0100

    chore: style the button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,21 @@
-function Button() {
-  return <div></div>;
-}
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: var(--color-red);
+
+  color: var(--color-white);
+
+  text-align: center;
+
+  padding: 1.2rem 2rem;
+
+  transition: all 0.2s;
+
+  &:hover {
+    color: var(--color-blue-dark);
+
+    background-color: var(--color-white);
+  }
+`;
 
 export default Button;
